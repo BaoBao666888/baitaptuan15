82f6f73d75238684f8928cf153bf90330fbcee83
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2024-11-28 14:05:54 +0700

    upload

---
+++ baitaptuan15/src/main/java/com/ltweb/Baitaptuan15Application.java
@@ -0,0 +1,13 @@
+package com.ltweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitaptuan15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baitaptuan15Application.class, args);
+	}
+
+}
---
+++ baitaptuan15/src/test/java/com/ltweb/Baitaptuan15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baitaptuan15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
